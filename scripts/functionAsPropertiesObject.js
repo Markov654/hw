79cbb88d7c62e2be4cdd0d20c781df9df4a9e888
Markov654 <iliq_markov@abv.bg>
2024-05-30 16:33:47 +0300

    hw

---
+++ scripts/functionAsPropertiesObject.js
@@ -0,0 +1,25 @@
+// TASK: define object 'calc', with next properties and values:
+// 'add' - a function, which returns the sum of 2 numbers
+// 'sub' - a function, which returns the subtraction of 2 numbers
+// 'mult' - a function, which returns the multiplication of 2 numbers
+// 'div' - a function, which returns the division of 2 numbers
+
+// ---> YOUR CODE HERE <---
+
+
+// TEST:
+let calc={
+
+    x: 6,
+    y:3,
+    
+}
+
+
+
+
+   
+console.log((calc.x + calc.y)); // 9
+console.log((calc.x - calc.y)); // 3
+console.log((calc.x * calc.y)); // 18
+console.log((calc.x / calc.y)); // 2
